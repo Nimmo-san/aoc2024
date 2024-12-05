75795726f56dcae1c162e467486e89171b40c459
Nimmo-san <nat25oloman@gmail.com>
2024-12-05 13:35:38 +0000

    rewrote day5 part 1/2 logic to use toplogical sorting instead

--- day5/day5.py
+++ day5/day5.py
@@ -1,7 +1,7 @@
+from collections import defaultdict, deque
+
 
 filename = 'day5/day5.txt'
-
-
 with open(filename, mode='r') as file:
     sections = file.read().strip().split("\n\n")
 
@@ -10,45 +10,70 @@ with open(filename, mode='r') as file:
     updates = [list(map(int, update.split(',')))  for update in sections[1].split('\n')]
 
 
-valid_updates = []
-invalid_updates = []
+# precedence = {}
+# valid_updates = []
+# invalid_updates = []
+
+# for _ in range(len(rules)):
+#     for a, b in rules:
+#         precedence[a] = precedence.get(a, 0)
+#         precedence[b] = max(precedence.get(b, 0), precedence[a] + 1)
+
+# for update in updates:
+#     valid = True
+#     for num1, num2 in rules:
+#         if num1 in update and num2 in update:
+#             pos_num1 = update.index(num1)
+#             pos_num2 = update.index(num2)
+
+#             if pos_num1 > pos_num2:
+#                 valid = False
+#                 break
+#     if valid:
+#         valid_updates.append(update)
+#     else:
+#         invalid_updates.append(update)
+
+# middle_pages = sum([update[len(update) // 2] for update in valid_updates])
+# updated_invalid = [sorted(update, key=lambda x: precedence.get(x, float('inf'))) for update in invalid_updates]
+# middle_updated_pages = [update[len(update) // 2] for update in updated_invalid]
+
+
+def optimized_topological_sort(update):
+    subgraph = defaultdict(list)
+    sub_in_degree = defaultdict(int)
+    
+    for a, b in rules:
+        if a in update and b in update:
+            subgraph[a].append(b)
+            sub_in_degree[b] += 1
+            sub_in_degree[a]
+
+    # Performing topological sort
+    queue = deque([node for node in update if sub_in_degree[node] == 0])
+    sorted_order = []
+
+    while queue:
+        current = queue.popleft()
+        sorted_order.append(current)
+        for neighbor in subgraph[current]:
+            sub_in_degree[neighbor] -= 1
+            if sub_in_degree[neighbor] == 0:
+                queue.append(neighbor)
+
+    return sorted_order
+
+correct_updates = []
+reordered_updates = []
 
 for update in updates:
-    valid = True
-    for rule in rules:
-        num1, num2 = rule
-        if num1 in update and num2 in update:
-            pos_num1 = update.index(num1)
-            pos_num2 = update.index(num2)
+    allowed_order = optimized_topological_sort(update)
+    if update == allowed_order:
+        correct_updates.append(update)
+    else:
+        reordered_updates.append(allowed_order)
 
-            if pos_num1 > pos_num2:
-                valid = False
-                break
+correct_middle_sum = sum(update[len(update) // 2] for update in correct_updates)
+reordered_middle_sum = sum(update[len(update) // 2] for update in reordered_updates)
 
-
-    if valid:
-        valid_updates.append(update)
-    
-    if not valid:
-        invalid_updates.append(update)
-    
-# print(valid_updates)
-
-
-# middle_pages = [update[len(update) // 2] for update in valid_updates]
-# print(sum(middle_pages))
-
-precedence = {}
-for _ in range(len(rules)):
-    for a, b in rules:
-        precedence[a] = precedence.get(a, 0)
-        precedence[b] = max(precedence.get(b, 0), precedence[a] + 1)
-
-
-updated_invalid = [sorted(update, key=lambda x: precedence.get(x, float('inf'))) for update in invalid_updates]
-
-# print(updated_invalid)
-middle_updated_pages = [update[len(update) // 2] for update in updated_invalid]
-print(sum(middle_updated_pages))
-        
-            
+print(correct_middle_sum, reordered_middle_sum)
