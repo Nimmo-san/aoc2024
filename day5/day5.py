ac96200c0e77a54af2c18e7ad27fb914e32dcda7
Nimmo-san <nat25oloman@gmail.com>
2024-12-05 12:45:48 +0000

    solved day5 part2 with sample

--- day5/day5.py
+++ day5/day5.py
@@ -11,6 +11,7 @@ with open(filename, mode='r') as file:
 
 
 valid_updates = []
+invalid_updates = []
 
 for update in updates:
     valid = True
@@ -24,13 +25,30 @@ for update in updates:
                 valid = False
                 break
 
+
     if valid:
         valid_updates.append(update)
+    
+    if not valid:
+        invalid_updates.append(update)
     
 # print(valid_updates)
 
 
-middle_pages = [update[len(update) // 2] for update in valid_updates]
-print(sum(middle_pages))
+# middle_pages = [update[len(update) // 2] for update in valid_updates]
+# print(sum(middle_pages))
 
+precedence = {}
+for _ in range(len(rules)):
+    for a, b in rules:
+        precedence[a] = precedence.get(a, 0)
+        precedence[b] = max(precedence.get(b, 0), precedence[a] + 1)
+
+
+updated_invalid = [sorted(update, key=lambda x: precedence.get(x, float('inf'))) for update in invalid_updates]
+
+# print(updated_invalid)
+middle_updated_pages = [update[len(update) // 2] for update in updated_invalid]
+print(sum(middle_updated_pages))
+        
             
