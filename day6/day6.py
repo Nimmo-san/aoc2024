80953a72c0b896fdc87e19c88347775fc7f3222d
Nimmo-san <nat25oloman@gmail.com>
2024-12-06 23:54:19 +0000

    abandoning day6 part2, need sleep :D

--- day6/day6.py
+++ day6/day6.py
@@ -1,13 +1,4 @@
 
-# Debugging purposes
-def print_grid(grid, guard_pos, facing_direction):
-    symbols = ['^', '>', 'v', '<']
-    temp_grid = [row[:] for row in grid]
-    temp_grid[guard_pos[0]][guard_pos[1]] = symbols[facing_direction]
-    for row in temp_grid:
-        print("".join(row))
-    print("\n")
-
 filename = "day6/day6.txt"
 
 with open(filename, mode='r') as file:
@@ -16,8 +7,8 @@ with open(filename, mode='r') as file:
 directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 direction_map = {'^': 0, '>': 1, 'v': 2, '<': 3}
 
-rows = len(grid)
-cols = len(grid[0])
+rows = len(grid[0])
+cols = len(grid)
 
 start_pos = None
 facing_direction = 0
@@ -32,29 +23,80 @@ for row in range(rows):
     if start_pos:
         break
 
-current_pos = start_pos
-visited = set()
-visited.add(current_pos)
-step_limit = 100000
-steps = 0
-next_pos = current_pos
+# FOR PART 1
+# Debugging purposes
+# def print_grid(grid, guard_pos, facing_direction):
+#     symbols = ['^', '>', 'v', '<']
+#     temp_grid = [row[:] for row in grid]
+#     temp_grid[guard_pos[0]][guard_pos[1]] = symbols[facing_direction]
+#     for row in temp_grid:
+#         print("".join(row))
+#     print("\n")
 
-while (0 <= next_pos[0] < rows and 0 <= next_pos[1] < cols):
+# current_pos = start_pos
 
-    # print(f"Step: {steps}, Current Pos: {current_pos}, Facing: {facing_direction}, Next Pos towards: {directions[facing_direction]}")
+# visited = set()
+# visited.add(start_pos)
 
-    # print_grid(grid, current_pos, facing_direction)
+# next_pos = start_pos
 
-    dr, dc = directions[facing_direction]
-    next_pos = (current_pos[0] + dr, current_pos[1] + dc)
+# while (0 <= next_pos[0] < rows and 0 <= next_pos[1] < cols):
 
-    if not (0 <= next_pos[0] < rows and 0 <= next_pos[1] < cols) or grid[next_pos[0]][next_pos[1]] == '#':
-        facing_direction = (facing_direction + 1) % 4
-        # print(f"Blocked. Turned to Facing: {facing_direction}")
-    else:
-        current_pos = next_pos
-        visited.add(current_pos)
-        # print(f"Moved to {current_pos}")
+#     # print(f"Step: {steps}, Current Pos: {current_pos}, Facing: {facing_direction}, Next Pos towards: {directions[facing_direction]}")
 
-    # steps += 1
-print(len(visited))
+#     # print_grid(grid, current_pos, facing_direction)
+
+#     dr, dc = directions[facing_direction]
+#     next_pos = (current_pos[0] + dr, current_pos[1] + dc)
+
+#     if not (0 <= next_pos[0] < rows and 0 <= next_pos[1] < cols) or grid[next_pos[0]][next_pos[1]] == '#':
+#         facing_direction = (facing_direction + 1) % 4
+#         # print(f"Blocked. Turned to Facing: {facing_direction}")
+#     else:
+#         current_pos = next_pos
+#         visited.add(current_pos)
+#         # print(f"Moved to {current_pos}")
+
+#     # steps += 1
+# print(len(visited))
+
+
+# FOR PART 2
+def simulate_guard_loop(grid, start_pos, facing_direction):
+    current_pos = start_pos
+    visited = set()
+    loop_positions = set()
+    steps = 0
+    max_steps = 100000
+
+    while steps < max_steps:
+        if (current_pos, facing_direction) in visited:
+            loop_positions.add(current_pos)
+            return True
+
+        visited.add((current_pos, facing_direction))
+
+        dr, dc = directions[facing_direction]
+        next_pos = (current_pos[0] + dr, current_pos[1] + dc)
+        if not (0 <= next_pos[0] < rows and 0 <= next_pos[1] < cols) or grid[next_pos[0]][next_pos[1]] == '#':
+            facing_direction = (facing_direction + 1) % 4
+        else:
+            current_pos = next_pos
+        
+        steps += 1
+    
+    grid[current_pos[0]][current_pos[1]] = '.'
+    return False
+
+
+valid_loops = 0
+
+for row in range(rows):
+    for col in range(cols):
+        if grid[row][col] == '.' and (row, col) != start_pos:
+            grid[row][col] = '#'
+            if simulate_guard_loop(grid, start_pos, facing_direction):
+                valid_loops += 1
+            grid[row][col] = '.'
+
+print(valid_loops)
