14a0510355fe9c15d9fe7992b75ef9f3ce0b41f0
Nimmo-san <nat25oloman@gmail.com>
2024-12-08 21:23:06 +0000

    solved day8 part 2 using queue

--- day8/day8.py
+++ day8/day8.py
@@ -24,6 +24,32 @@ for row in range(rows):
 
 antinodes = set()
 
+# FOR PART 1
+# for freq, positions in antennas.items():
+#     n = len(positions)
+#     for i in range(n - 1):
+#         for j in range(i + 1, n):
+#             loc_diff = (
+#                 positions[i][0] - positions[j][0],
+#                 positions[i][1] - positions[j][1]
+#             )
+#             # print(loc_diff)
+#             for loc in (positions[i], positions[j]):
+#                 new_loc = (loc[0] + loc_diff[0], loc[1] + loc_diff[1])
+
+#                 if 0 <= new_loc[0] < rows and 0 <= new_loc[1] < cols and grid[new_loc[0]][new_loc[1]] != freq:
+#                     antinodes.add(new_loc)
+#                     grid[new_loc[0]][new_loc[1]] = '#'
+
+#                 # maybe consider a neg
+#                 new_loc_neg = (loc[0] - loc_diff[0], loc[1] - loc_diff[1])
+#                 if 0 <= new_loc_neg[0] < rows and 0 <= new_loc_neg[1] < cols and grid[new_loc_neg[0]][new_loc_neg[1]] != freq:
+#                     antinodes.add(new_loc_neg)
+#                     grid[new_loc_neg[0]][new_loc_neg[1]] = '#'
+
+
+
+# FOR PART 2
 for freq, positions in antennas.items():
     n = len(positions)
     for i in range(n - 1):
@@ -32,19 +58,27 @@ for freq, positions in antennas.items():
                 positions[i][0] - positions[j][0],
                 positions[i][1] - positions[j][1]
             )
+            visited = set()
+            queue = deque([positions[i], positions[j]])
+
             # print(loc_diff)
-            for loc in (positions[i], positions[j]):
+            while queue:
+                loc = queue.popleft()
                 new_loc = (loc[0] + loc_diff[0], loc[1] + loc_diff[1])
 
-                if 0 <= new_loc[0] < rows and 0 <= new_loc[1] < cols and grid[new_loc[0]][new_loc[1]] != freq:
+                if 0 <= new_loc[0] < rows and 0 <= new_loc[1] < cols and new_loc not in visited:
+                    visited.add(new_loc)
+                    queue.append(new_loc)
                     antinodes.add(new_loc)
                     grid[new_loc[0]][new_loc[1]] = '#'
 
                 # maybe consider a neg
                 new_loc_neg = (loc[0] - loc_diff[0], loc[1] - loc_diff[1])
-                if 0 <= new_loc_neg[0] < rows and 0 <= new_loc_neg[1] < cols and grid[new_loc_neg[0]][new_loc_neg[1]] != freq:
+                if 0 <= new_loc_neg[0] < rows and 0 <= new_loc_neg[1] < cols and new_loc_neg not in visited:
+                    visited.add(new_loc_neg)
+                    queue.append(new_loc_neg)
                     antinodes.add(new_loc_neg)
                     grid[new_loc_neg[0]][new_loc_neg[1]] = '#'
-                
+
 
 print(len(antinodes))
