0a368d2cc03b2c697775b4f00724e91151949895
Nimmo-san <nat25oloman@gmail.com>
2024-12-09 13:35:36 +0000

    modularised day9 part 1 into functions

--- day9/day9.py
+++ day9/day9.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 
 filename = 'day9/day9.txt'
 
@@ -6,38 +5,38 @@ with open(filename, mode='r') as file:
     disk = file.read()
 
 
-fragment = []
-renewed = False
-index = 0
 
-for i, char in enumerate(disk):
-    # print(i, char)
-    for j in range(int(char)):
-        if i % 2 == 0:
-            renewed = True
-            fragment.append(index)
+def disk_mapping(fragment):
+    disk_mapped = []
+    file_id = 0
+    for i in range(0, len(fragment), 2):
+        file_length = int(fragment[i])
+        free_space = int(fragment[i + 1] if i + 1 < len(fragment) else 0)
+
+        disk_mapped.extend([str(file_id)] * file_length)
+        disk_mapped.extend(['.'] * free_space)
+        file_id += 1
+    return disk_mapped
+
+
+def compact(fragment):
+    while '.' in fragment:
+        leftmost = fragment.index('.')
+        num = fragment[::-1].index(next(c for c in reversed(fragment) if c != '.'))
+        rightmost = len(fragment) - 1 - num
+
+        if rightmost > leftmost:
+            fragment[leftmost], fragment[rightmost] = fragment[rightmost], '.'
         else:
-            fragment.append('.')
+            break
+    return fragment
 
-    if renewed:
-        index += 1
-        renewed = False
-
-
-
-while '.' in fragment:
-    leftmost = fragment.index('.')
-    num = fragment[::-1].index(next(c for c in reversed(fragment) if c != '.'))
-    rightmost = len(fragment) - 1 - num
-
-    if rightmost > leftmost:
-        fragment[leftmost], fragment[rightmost] = fragment[rightmost], '.'
-    else:
-        break
 
 total = 0
+fragment = disk_mapping(disk)
+result = compact(fragment)
 
-for i, num in enumerate(fragment):
+for i, num in enumerate(result):
     if num != '.':
         total += i * int(num)
     
