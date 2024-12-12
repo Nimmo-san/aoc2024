ade2994cc288537d5ddb000a40a85e52039ddedd
Nimmo-san <nat25oloman@gmail.com>
2024-12-12 12:30:42 +0000

    solved day 11 part 2

--- day11/day11.py
+++ day11/day11.py
@@ -1,4 +1,4 @@
-
+from functools import cache
 
 filename = "day11/input.txt"
 
@@ -8,23 +8,46 @@ with open(filename, mode='r') as file:
     stones = [int(num) for num in input]
 
 
-for _ in range(25):
-    output = []
-    for stone in stones:
-        string_stone = str(stone)
-        size = len(string_stone)
-        if stone == 0:
-            output.append(1)
-            continue
+# for _ in range(25):
+#     output = []
+#     for stone in stones:
+#         string_stone = str(stone)
+#         size = len(string_stone)
+#         if stone == 0:
+#             output.append(1)
+#             continue
 
-        if size % 2 == 0:
-            output.append(int(string_stone[:size // 2]))
-            output.append(int(string_stone[size // 2:]))
-        else:
-            output.append(2024 * stone)
-    stones = output
+#         if size % 2 == 0:
+#             output.append(int(string_stone[:size // 2]))
+#             output.append(int(string_stone[size // 2:]))
+#         else:
+#             output.append(2024 * stone)
+#     stones = output
     # print(stones)
 
-print(len(stones))
+# print(len(stones))
+
+@cache
+def find_answer_recursively(num, steps):
+    if steps == 0:
+        return 1
+    
+    if num == 0:
+        return find_answer_recursively(1, steps - 1)
+    
+    
+    string_stone = str(num)
+    size = len(string_stone)
+    if size % 2 == 0:
+        return find_answer_recursively(int(string_stone[:size // 2]), steps - 1) + find_answer_recursively(int(string_stone[size // 2:]), steps - 1)
+    else:
+        return find_answer_recursively(2024 * num, steps - 1)
+
+
+result = 0
+for stone in stones:
+    result += find_answer_recursively(stone, 75)
+
+print(result)
 
 
