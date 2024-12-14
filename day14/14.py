184cc9151076e3d022d46ee782036f506f051b86
Nimmo-san <nat25oloman@gmail.com>
2024-12-14 20:19:44 +0000

    solved day 14 part 2

--- day14/14.py
+++ day14/14.py
@@ -1,5 +1,5 @@
 import re
-import sys
+import sys  # noqa: F401
 
 
 filename = "day14/input.txt"
@@ -18,49 +18,50 @@ for line in open(filename):
     robots.append(tuple(map(int, re.findall(r'-?\d+', line))))
 
 
-output = []
-q1 = q2 = q3 = q4 = 0
 # for px, py, vx, vy in robots:
 #     x_mod = (px + vx * 100) % W
 #     y_mod = (py + vy * 100) % H
 #     output.append((x_mod, y_mod))
 
 
+# min_area = float('inf')
+min_safety_factor = float('inf')
+min_time = None
 
-step = 0
-while True:
+for step in range(W * H):
+    output = []
+    q1 = q2 = q3 = q4 = 0
     for px, py, vx, vy in robots:
         x_mod = (px + vx * step) % W
         y_mod = (py + vy * step) % H
         output.append((x_mod, y_mod))
 
-        min_x = min(p[0] for p in output)
-        max_x = max(p[0] for p in output)
-        min_y = min(p[1] for p in output)
-        max_y = max(p[1] for p in output)
+    # xs = [p[0] for p in output]
+    # ys = [p[1] for p in output]
 
-        # print(min_x, max_x, min_y, max_y)
-        if (max_x < min_x) < 50 and (max_y - min_y) < 50:
-            grid = [['.' for _ in range(W)] for _ in range(H)]
-            for x, y in output:
-                grid[y][x] = '#'
-            for row in grid:
-                print(''.join(row))
-            print()
-        # print(px, py)
-        # Seems like this check doesnt matter
-        # if px == (W - 1)//2 or py == (H - 1)//2: continue  # noqa: E701
+    # width = max(xs) - min(xs) + 1
+    # height = max(ys) - min(ys) + 1
 
-        # if px < (W - 1)//2 and py < (H - 1)//2:
-        #     q1 += 1
-        # elif px > (W - 1)//2 and py < (H - 1)//2:
-        #     q2 += 1
-        # elif px < (W - 1)//2 and py > (H - 1)//2:
-        #     q3 += 1
-        # elif px > (W - 1)//2 and  py > (H - 1)//2:
-        #     q4 += 1
+    # area = width * height
 
-    # print(q1 * q2 * q3 * q4)
-    print(step)
-    step += 1
+    # if area < min_area:
+    #     min_area = area
+    #     min_time = step
 
+    for px, py in output:
+        if px < (W - 1)//2 and py < (H - 1)//2:
+            q1 += 1
+        elif px > (W - 1)//2 and py < (H - 1)//2:
+            q2 += 1
+        elif px < (W - 1)//2 and py > (H - 1)//2:
+            q3 += 1
+        elif px > (W - 1)//2 and  py > (H - 1)//2:
+            q4 += 1
+
+    safety_factor = q1 * q2 * q3 * q4
+
+    if safety_factor < min_safety_factor:
+        min_safety_factor = safety_factor
+        min_time = step
+
+print(min_safety_factor, min_time)
