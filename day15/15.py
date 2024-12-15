f3e7639be17f955643fcf9310d674051ca796c86
Nimmo-san <nat25oloman@gmail.com>
2024-12-15 19:54:57 +0000

    solved day 15 part 2, took too long :O

--- day15/15.py
+++ day15/15.py
@@ -1,72 +1,140 @@
-
+import copy
 
 filename = "day15/input.txt"
 
 first_half, second_half = open(filename).read().split("\n\n")
 
-grid = [list(line) for line in first_half.splitlines()]
+
+char_replace = {
+    '#': '##',
+    'O': '[]',
+    '.': '..',
+    '@': '@.'
+}
+
+grid_part1 = [list(line) for line in first_half.splitlines()]
+grid_part2 = [list(''.join(char_replace[char] for char in line)) for line in first_half.splitlines()]
 moves = second_half.replace("\n", "")
 
+# print('\n'.join(grid_part2))
 
-rows = len(grid)
-cols = len(grid[0])
+# exit(0)
 
-for row in range(rows):
-    for col in range(cols):
-        if grid[row][col] == "@":
-            break
-    else:
-        continue
-    break
+def part1(grid, moves):
+    rows = len(grid)
+    cols = len(grid[0])
 
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == "@":
+                break
+        else:
+            continue
+        break
 
+    for move in moves:
+        dr = {'^': -1, 'v': 1}.get(move, 0)
+        dc = {'<': -1, '>': 1}.get(move, 0)
 
-for move in moves:
-    dr = {'^': -1, 'v': 1}.get(move, 0)
-    dc = {'<': -1, '>': 1}.get(move, 0)
+        targets = [(row, col)]
+        green = True
+        cr = row
+        cc = col
+        while True:
+            cr += dr
+            cc += dc
 
-    targets = [(row, col)]
-    green = True
-    cr = row
-    cc = col
-    while True:
-        cr += dr
-        cc += dc
+            if grid[cr][cc] == '#':
+                green = False
+                break
 
-        if grid[cr][cc] == '#':
-            green = False
-            break
+            if grid[cr][cc] == 'O':
+                targets.append((cr, cc))
+            
+            if grid[cr][cc] == '.':
+                break
 
-        if grid[cr][cc] == 'O':
-            targets.append((cr, cc))
+        if not green:
+            continue
+
+        grid[row][col] = '.'
+
+        grid[row + dr][col + dc] = '@'
+
+        for br, bc in targets[1:]:
+            grid[br + dr][bc + dc] = 'O'
         
-        if grid[cr][cc] == '.':
-            break
+        row += dr
+        col += dc
 
-    if not green:
-        continue
+    # for line in grid:
+    #     print(*line)
 
-    grid[row][col] = '.'
+    # total = 0
+    # for row in range(rows):
+    #     for col in range(cols):
+    #         if grid[row][col] == 'O':
+    #             total += 100 * row + col
 
-    grid[row + dr][col + dc] = '@'
+    return sum(100 * row + col for row in range(rows) for col in range(cols) if grid[row][col] == 'O')
 
-    for br, bc in targets[1:]:
-        grid[br + dr][bc + dc] = 'O'
+
+def part2(grid, moves):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == "@":
+                break
+        else:
+            continue
+        break
     
-    row += dr
-    col += dc
+    # print("initial pos of robot",row, col)
+    for move in moves:
+        dr = {'^': -1, 'v': 1}.get(move, 0)
+        dc = {'<': -1, '>': 1}.get(move, 0)
+        targets = [(row, col)]
+        green = True
+        for cr, cc in targets:
+            nr = cr + dr
+            nc = cc + dc
+            
+            if (nr, nc) in targets:
+                continue
 
+            char = grid[nr][nc]
+            if char == '#':
+                green = False
+                break
 
-# for line in grid:
-#     print(*line)
+            if char == '[':
+                targets.append((nr, nc))
+                targets.append((nr, nc + 1))
+            
+            if char == ']':
+                targets.append((nr, nc))
+                targets.append((nr, nc - 1))
+        if not green:
+            continue
+        
+        grid_copy = copy.deepcopy(grid)
+        # print(grid[row][col])
+        grid[row][col] = "."
+        grid[row + dr][col + dc] == '@'
 
-total = 0       
+        for br, bc in targets[1:]:
+            grid[br][bc] = "."
+        
+        for br, bc in targets[1:]:
+            grid[br + dr][bc + dc] = grid_copy[br][bc]
 
-for row in range(rows):
-    for col in range(cols):
-        if grid[row][col] == 'O':
-            total += 100 * row + col
-
-print(total)
+        row += dr
+        col += dc
 
     
+    return sum(100 * row + col for row in range(rows) for col in range(cols) if grid[row][col] == '[')
+
+print(part2(grid_part2, moves))
+        
