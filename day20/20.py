9dd73e442eacc68d696617195f09badeec013307
Nimmo-san <nat25oloman@gmail.com>
2024-12-20 14:15:06 +0000

    grid input for day20 part 1

--- day20/20.py
+++ day20/20.py
@@ -1,5 +1,29 @@
 
+from pprint import pprint
 filename = "day20/input.txt"
 
 
+grid = [list(line) for line in open(filename).read().strip().split('\n')]
 
+# pprint(grid)
+
+rows = len(grid)
+cols = len(grid[0])
+
+
+sr,sc = 0,0
+er,ec = 0,0
+
+for i in range(rows):
+    for j in range(cols):
+        if grid[i][j] == 'S':
+            sr,sc = i,j
+            grid[i][j] = '.'
+        elif grid[i][j] == 'E':
+            er,ec = i,j
+            grid[i][j] = '.'
+
+print(sr,sc)
+print(er,ec)
+
+pprint(grid)
